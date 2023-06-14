af3e23d96fe985bb64ceae7dd4f74430ac453825
hyun <hyunp2@illinois.edu>
2023-06-13 22:16:41 -0500

    dgx

--- submit_dgx.sh
+++ submit_dgx.sh
@@ -32,14 +32,14 @@ fi
 echo "JOB_NAME $1; MACHINE_NAME $2; FASTA_FILES $3; MAX_TEMPLATE_DATE $4; USE_PRECOMPUTED_MSAS $5; MODEL_PRESET $6; DB_PRESEET $7; NUM_PREDS_PER_MODEL $8; MODELS_TO_RELAX $9; GPU_DEVICES ${10}; CONTINUED_SIMULATION ${11} are chosen..."
 
 PYTHON=/Projects/ghaemi/Programs/Conda/envs/AF/bin/python3
-ALPHAFOLD_DIR=/Scr/alpha_fold
+ALPHAFOLD_DIR=/Scr/hyunpark/Alphafold
+TCBG_DIR=/Scr/alpha_fold
 DOCKER_NAME=hyunp2/alphafold_original
 
 qsub -q $MACHINE_NAME -N $JOB_NAME -j y -o ~/Jobs << EOF
-pushd $ALPHAFOLD_DIR/alphafold2
+pushd $ALPHAFOLD_DIR/alphafold
 
-$PYTHON $ALPHAFOLD_DIR/alphafold2/docker/run_docker.py --data_dir=$ALPHAFOLD_DIR/data_hyun_official --fasta_paths=$FASTA_FILES --max_template_date=$MAX_TEMPLATE_DATE --use_precomputed_msas=$USE_PRECOMPUTED_MSAS --model_preset $MODEL_PRESET --db_preset $DB_PRESET --num_multimer_predictions_per_model $NUM_PREDS_PER_MODEL --enable_gpu_relax=true --output_dir=$ALPHAFOLD_DIR/outputs --docker_image_name $DOCKER_NAME --models_to_relax=$MODELS_TO_RELAX --gpu_devices=$GPU_DEVICES --continued_simulation $CONTINUED_SIMULATION
-popd
+$PYTHON $ALPHAFOLD_DIR/alphafold/docker/run_docker.py --data_dir=$TCBG_DIR/data_hyun_official --fasta_paths=$FASTA_FILES --max_template_date=$MAX_TEMPLATE_DATE --use_precomputed_msas=$USE_PRECOMPUTED_MSAS --model_preset $MODEL_PRESET --db_preset $DB_PRESET --num_multimer_predictions_per_model $NUM_PREDS_PER_MODEL --enable_gpu_relax=true --output_dir=$TCBG_DIR/outputs --docker_image_name $DOCKER_NAME --models_to_relax=$MODELS_TO_RELAX --gpu_devices=$GPU_DEVICES --continued_simulation=$CONTINUED_SIMULATION
 EOF
 
 #JOBNAME="${1/%.namd/}"
