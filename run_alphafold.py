3e8b7aebfd5df760e0df9de3fa0b551c92b16cbb
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-06-14 09:57:39 -0500

    Update run_alphafold.py

--- run_alphafold.py
+++ run_alphafold.py
@@ -44,7 +44,7 @@ import numpy as np
 import jax
 
 import ray
-import itertools
+import functools
 # Internal import (7716).
 
 logging.set_verbosity(logging.INFO)
@@ -372,7 +372,7 @@ def predict_structure(
   
   outputs = list(zip(*outputs)) #->[(dic0, dic0...), (dic1, dic1...), ...]
   assert len(outputs) == 4, "There should be only four tuples ready..."
-  timings, unrelaxed_proteins, unrelaxed_pdbs, ranking_confidences = [itertools.reduce(ranking_confidences, out) for out in outputs] #->List[dict]
+  timings, unrelaxed_proteins, unrelaxed_pdbs, ranking_confidences = [functools.reduce(ranking_confidences, out) for out in outputs] #->List[dict]
   return timings, unrelaxed_proteins, unrelaxed_pdbs, ranking_confidences
   
 def structure_ranker( model_runners: Dict[str, model.RunModel],
