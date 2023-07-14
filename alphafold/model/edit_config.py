61eba1bdb912f56fc178184ce120d48dc0d62c76
hyunp2 <42776897+hyunp2@users.noreply.github.com>
2023-07-14 13:55:23 -0500

    Update edit_config.py

--- alphafold/model/edit_config.py
+++ alphafold/model/edit_config.py
@@ -40,62 +40,63 @@ def load_models_config(
     #     models_need_compilation = [1, 3] if use_templates else [3]
     
     model_runner = None
-    for model_number in model_build_order:
-        # if model_number in models_need_compilation:
+    # for model_number in model_build_order:
+    
+    # if model_number in models_need_compilation:
 
-        # get configurations
-        # model_config = config.model_config("model_" + str(model_number) + model_suffix)
-        model_config = config #from run_alphafold.py
-        # model_config.model.stop_at_score = float(stop_at_score)
-        # model_config.model.rank_by = rank_by
+    # get configurations
+    # model_config = config.model_config("model_" + str(model_number) + model_suffix)
+    model_config = config #from run_alphafold.py
+    # model_config.model.stop_at_score = float(stop_at_score)
+    # model_config.model.rank_by = rank_by
 
-        # set dropouts
-        model_config.model.global_config.eval_dropout = use_dropout
+    # set dropouts
+    model_config.model.global_config.eval_dropout = use_dropout
 
-        # set bfloat options
-        model_config.model.global_config.bfloat16 = use_bfloat16
-        
-        # set fuse options
-        model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_incoming.fuse_projection_weights = use_fuse
-        model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_outgoing.fuse_projection_weights = use_fuse
-        
-        # if "multimer" in model_suffix or model_number in [1,2]:
-        model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_incoming.fuse_projection_weights = use_fuse
-        model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_outgoing.fuse_projection_weights = use_fuse
-                    
-        # set number of sequences options
-        if max_seq is not None:
-            if "multimer" in model_suffix:
-                model_config.model.embeddings_and_evoformer.num_msa = max_seq
-            else:
-                model_config.data.eval.max_msa_clusters = max_seq
-        
-        if max_extra_seq is not None:
-            if "multimer" in model_suffix:
-                model_config.model.embeddings_and_evoformer.num_extra_msa = max_extra_seq
-            else:
-                model_config.data.common.max_extra_msa = max_extra_seq
-
-        # disable some outputs if not being saved
-        if not save_all:
-            model_config.model.heads.distogram.weight = 0.0
-            model_config.model.heads.masked_msa.weight = 0.0
-            model_config.model.heads.experimentally_resolved.weight = 0.0
-
-        # set number of recycles and ensembles            
+    # set bfloat options
+    model_config.model.global_config.bfloat16 = use_bfloat16
+    
+    # set fuse options
+    model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_incoming.fuse_projection_weights = use_fuse
+    model_config.model.embeddings_and_evoformer.evoformer.triangle_multiplication_outgoing.fuse_projection_weights = use_fuse
+    
+    # if "multimer" in model_suffix or model_number in [1,2]:
+    model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_incoming.fuse_projection_weights = use_fuse
+    model_config.model.embeddings_and_evoformer.template.template_pair_stack.triangle_multiplication_outgoing.fuse_projection_weights = use_fuse
+                
+    # set number of sequences options
+    if max_seq is not None:
         if "multimer" in model_suffix:
-            if num_recycles is not None:
-                model_config.model.num_recycle = num_recycles
-            # model_config.model.embeddings_and_evoformer.use_cluster_profile = use_cluster_profile
-            model_config.model.num_ensemble_eval = num_ensemble
+            model_config.model.embeddings_and_evoformer.num_msa = max_seq
         else:
-            if num_recycles is not None:
-                model_config.data.common.num_recycle = num_recycles
-                model_config.model.num_recycle = num_recycles
-            model_config.data.eval.num_ensemble = num_ensemble
+            model_config.data.eval.max_msa_clusters = max_seq
+    
+    if max_extra_seq is not None:
+        if "multimer" in model_suffix:
+            model_config.model.embeddings_and_evoformer.num_extra_msa = max_extra_seq
+        else:
+            model_config.data.common.max_extra_msa = max_extra_seq
+
+    # disable some outputs if not being saved
+    if not save_all:
+        model_config.model.heads.distogram.weight = 0.0
+        model_config.model.heads.masked_msa.weight = 0.0
+        model_config.model.heads.experimentally_resolved.weight = 0.0
+
+    # set number of recycles and ensembles            
+    if "multimer" in model_suffix:
+        if num_recycles is not None:
+            model_config.model.num_recycle = num_recycles
+        # model_config.model.embeddings_and_evoformer.use_cluster_profile = use_cluster_profile
+        model_config.model.num_ensemble_eval = num_ensemble
+    else:
+        if num_recycles is not None:
+            model_config.data.common.num_recycle = num_recycles
+            model_config.model.num_recycle = num_recycles
+        model_config.data.eval.num_ensemble = num_ensemble
 
 
-        if recycle_early_stop_tolerance is not None:
-            model_config.model.recycle_early_stop_tolerance = recycle_early_stop_tolerance
+    if recycle_early_stop_tolerance is not None:
+        model_config.model.recycle_early_stop_tolerance = recycle_early_stop_tolerance
             
     return model_config
